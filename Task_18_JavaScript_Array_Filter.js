08fd24b33da61ad1cccc47024ad6dd3465cef264
cleanerro <romastepanyuk@gmail.com>
2022-01-26 16:27:40 +0200

    Task_18_JavaScript_Array_Filter

---
+++ Task_18_JavaScript_Array_Filter.js
@@ -0,0 +1,14 @@
+function getEvenNumbers(numbersArray){
+   return numbersArray.filter(el => el%2===0 )
+}
+
+//analog
+/*function getEvenNumbers(numbersArray){
+  return numbersArray.filter(filterArray);
+}
+
+function filterArray(numbersArray) {
+  return numbersArray%2 === 0 ;
+}*/
+
+
